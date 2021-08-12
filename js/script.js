3654f83fa36bcfd5025629f0b80c50fd8fb7355f
TakiKauser <takikauserbadhon@gmail.com>
2021-08-13 00:40:31 +0600

    ...ClassName, querySelectorAll

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 // console.log(33);
+
+// exploring getElementsByTagName
 const blogTitles = document.getElementsByTagName("h3");
 // console.log(blogTitles);
 for (const h3 of blogTitles){
@@ -9,6 +11,8 @@ const blogDetails = document.getElementsByTagName("p");
 for (const p of blogDetails){
     console.log(p.innerHTML);
 }
+
+// exploring getElementByID
 const firstTitle = document.getElementById("first-title");
 firstTitle.style.backgroundColor = "rgba(255, 166, 0, 0.452)";
 
@@ -17,3 +21,18 @@ secondTitle.style.backgroundColor = "rgba(165, 42, 42, 0.356)";
 
 const thirdTitle = document.getElementById("third-title");
 thirdTitle.style.backgroundColor = "rgba(255, 68, 0, 0.411)";
+
+// exploring getElementsByclass
+const blogs = document.getElementsByClassName("blog");
+for (const blog of blogs){
+    // console.log(blog);
+    blog.style.border = "5px solid teal";
+    blog.style.borderRadius = "10px";
+    blog.style.margin = "3%";
+    blog.style.padding = "10px";
+}
+
+const titles = document.getElementsByClassName("title");
+for (const title of titles){
+    title.style.textAlign = "center";
+}
