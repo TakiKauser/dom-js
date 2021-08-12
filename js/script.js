ef35a0a4890a05181e6f946693e7f0b5c6aebadd
TakiKauser <takikauserbadhon@gmail.com>
2021-08-13 00:06:02 +0600

    ...Id

--- js/script.js
+++ js/script.js
@@ -9,3 +9,11 @@ const blogDetails = document.getElementsByTagName("p");
 for (const p of blogDetails){
     console.log(p.innerHTML);
 }
+const firstTitle = document.getElementById("first-title");
+firstTitle.style.backgroundColor = "rgba(255, 166, 0, 0.452)";
+
+const secondTitle = document.getElementById("second-title");
+secondTitle.style.backgroundColor = "rgba(165, 42, 42, 0.356)";
+
+const thirdTitle = document.getElementById("third-title");
+thirdTitle.style.backgroundColor = "rgba(255, 68, 0, 0.411)";
